9467cb572c11254e7f6cd1ca400570e6c78bd303
ishita-git16 <54357950+ishita-git16@users.noreply.github.com>
2021-07-21 15:16:58 +0530

    IAM first task - Login Form

---
+++ practice.js
@@ -0,0 +1,15 @@
+'use strict';
+
+class Polygon {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+    getArea() {
+        return this.height * this.width;
+    }
+}
+
+const square = new Polygon(10, 10);
+
+console.log(square.getArea());
